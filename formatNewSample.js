bda5a645555959b49e1f2c317a56755ea7bc64a7
Charlie Gerard <annecharlotte.gerard@gmail.com>
2019-05-14 09:41:35 +0100

    first commit, few gestures working

---
+++ formatNewSample.js
@@ -0,0 +1,48 @@
+const lineReader = require('line-reader');
+var fs = require('fs');
+const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-node');
+
+function readNewFile(file) {
+    let trimmedContent = [];
+  
+    return new Promise((resolve, reject) => {
+        fs.readFile(`data/new/${file}`, "utf8", (err, data) => {
+            if (err){
+                reject(err);
+            } else{ 
+                lineReader.eachLine(`./data/new/${file}`, function(line) {  
+                    let truncatedLine = line.substr(line.indexOf('START ') + 6, line.indexOf('END')-7);
+                    let arrayFromLine = truncatedLine.split(" ");
+                    let formattedLine = arrayFromLine.map(arrayItem => parseFloat(arrayItem));
+                    trimmedContent.push(formattedLine);
+  
+                    let concatArray = trimmedContent.reduce((acc, val) => acc.concat(val), []);
+  
+                    if(concatArray.length === 360){
+                        resolve(concatArray)
+                    }
+                });
+            } 
+        });
+    });
+  }
+  
+  const readNewDir = () => 
+    new Promise((resolve, reject) => fs.readdir(`./data/new/`, "utf8", (err, data) => err ? reject(err) : resolve(data)));
+  
+  const getNewData = async() => {
+    const filenames = await readNewDir();
+  
+    return new Promise((resolve, reject) => {
+        filenames.map(async file => { // 75 times
+            let formattedNewData = await readNewFile(file); 
+ 
+            resolve(formattedNewData);
+        })
+    })
+  }
+
+module.exports = async() => {
+    return await getNewData();
+}
---
+++ live-game.js
@@ -0,0 +1,46 @@
+const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-node');
+var daydream = require('daydream-node')();
+
+let liveData = [];
+let predictionDone = false;
+
+let model;
+const gestureClasses = ['hadoken', 'punch', 'uppercut'];
+
+const init = async () => {
+    model = await tf.loadLayersModel('file://model-game/model.json');
+
+    getDaydreamData();
+}
+
+const getDaydreamData = () => {
+    daydream.onStateChange(function(data){
+        if(data.isClickDown){
+            predictionDone = false;
+            if(liveData.length < 300){
+                liveData.push(data.xAcc, data.yAcc, data.zAcc, data.xGyro, data.yGyro, data.zGyro)
+            }
+        } else {
+            if(!predictionDone && liveData.length){
+                predictionDone = true;
+                predict(model, liveData);
+                liveData = [];
+            }
+        }
+    });
+}
+
+const predict = (model, newSampleData) => {
+    tf.tidy(() => {
+        const inputData = newSampleData;
+        const input = tf.tensor2d([inputData], [1, 300]);
+        const predictOut = model.predict(input);
+        const logits = Array.from(predictOut.dataSync());
+        const winner = gestureClasses[predictOut.argMax(-1).dataSync()[0]];
+    
+        console.log("GESTURE: ", winner);
+    });
+}
+
+init();
